939d89c2a878b7851725ff9b98a7f1cf2b62a047
blackboxai-deploy <deploy@blackboxai.dev>
2025-09-05 21:54:11 +0530

    Add src/lib/utils.ts

---
+++ src/lib/utils.ts
@@ -0,0 +1,29 @@
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export function formatNumber(num: number): string {
+  if (num >= 1000000) {
+    return (num / 1000000).toFixed(1) + 'M'
+  } else if (num >= 1000) {
+    return (num / 1000).toFixed(1) + 'K'
+  }
+  return num.toString()
+}
+
+export function formatDate(date: Date): string {
+  const now = new Date()
+  const diffInHours = Math.floor((now.getTime() - date.getTime()) / (1000 * 60 * 60))
+  
+  if (diffInHours < 24) {
+    return `${diffInHours}h ago`
+  } else if (diffInHours < 24 * 7) {
+    const days = Math.floor(diffInHours / 24)
+    return `${days}d ago`
+  } else {
+    return date.toLocaleDateString()
+  }
+}
